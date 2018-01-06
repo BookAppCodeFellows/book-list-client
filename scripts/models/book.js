e3f5fd0d752b55517d55d7e030e120ec773bc6bc
Rafael Malave <rafomalave@gmail.com>
2018-01-06 13:20:04 -0800

    fix call

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,7 +1,7 @@
 const __API_URL__ = 'https://dk-rm-nd-booklist.herokuapp.com/test';
-// const __API_URL__ = 'http:localhost:3000/test';
+//const __API_URL__ = 'http:localhost:3000/test';
 
-$.getJSON(__API_URL__)
+$.get(__API_URL__)
 .then(result => {
-    result.send(result);
+    console.log(result);
 }).catch(error => console.error(error));
