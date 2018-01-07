cf907e21b963184961b410ae8a03cef6e78e150b
Rafael Malave <rafomalave@gmail.com>
2018-01-06 16:06:07 -0800

    setup api call

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,7 +1,10 @@
-const __API_URL__ = 'https://dk-rm-nd-booklist.herokuapp.com/test';
+const __API_URL__ = 'https://dk-rm-nd-booklist.herokuapp.com/api/v1/books';
 //const __API_URL__ = 'http:localhost:3000/test';
 
-$.get(__API_URL__)
+$.getJSON(__API_URL__)
 .then(result => {
-    console.log(result);
+    result.forEach(book => {
+        $('#booklist').append(book.author);
+        console.log(result);
+    })
 }).catch(error => console.error(error));
